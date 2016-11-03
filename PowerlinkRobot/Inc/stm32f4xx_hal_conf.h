b113361ab05597e10c3e79d37d2c3f877eda4403
Jackie Gao <ghjackie@gmail.com>
2016-11-04 00:52:37 +0800

    small tweak for HAL_StatusTypeDef build issue.

--- PowerlinkRobot/Inc/stm32f4xx_hal_conf.h
+++ PowerlinkRobot/Inc/stm32f4xx_hal_conf.h
@@ -42,6 +42,17 @@
 
 #include "mxconstants.h" 
 
+/** 
+* @brief  HAL Status structures definition  
+*/  
+typedef enum 
+{
+  HAL_OK       = 0x00U,
+  HAL_ERROR    = 0x01U,
+  HAL_BUSY     = 0x02U,
+  HAL_TIMEOUT  = 0x03U
+} HAL_StatusTypeDef;
+
 /* Exported types ------------------------------------------------------------*/
 /* Exported constants --------------------------------------------------------*/
 
